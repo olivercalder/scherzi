efc63ab8cc093a0cab761dc66f66e5b1b2ae27cb
Oliver Calder <olivercalder@protonmail.ch>
2021-10-23 14:47:50 -0500

    Wrote merge sort in python and scheme.

---
+++ merge_sort.py
@@ -0,0 +1,17 @@
+def merge(left, right):
+    if len(left) <= 0:
+        return right
+    if len(right) <= 0:
+        return left
+    if left[0] < right[0]:
+        return [left[0]] + merge(left[1:], right)
+    else:
+        return [right[0]] + merge(left, right[1:])
+
+def merge_sort(array):
+    if len(array) < 2:
+        return array
+    return merge(merge_sort(array[:len(array) // 2]), merge_sort(array[len(array) // 2:]))
+
+unsorted = [5, 3, 4, 9, 8, 1, 7, 2, 0, 6]
+print(merge_sort(unsorted))
---
+++ merge_sort.scm
@@ -0,0 +1,31 @@
+(define before-n
+  (lambda (array n)
+    (if (or (<= n 0) (null? array))
+      '()
+      (cons (car array) (before-n (cdr array) (- n 1))))))
+
+(define after-n
+  (lambda (array n)
+    (cond ((<= n 0) array)
+          ((null? array) '())
+          (else (after-n (cdr array) (- n 1))))))
+
+(define merge
+  (lambda (left right)
+    (cond ((null? left) right)
+          ((null? right) left)
+          ((< (car left) (car right))
+            (cons (car left) (merge (cdr left) right)))
+          (else
+            (cons (car right) (merge left (cdr right)))))))
+
+(define merge-sort
+  (lambda (array)
+    (if (<= (length array) 1) array
+      (let ((middle (inexact->exact (floor (/ (length array) 2)))))
+        (merge
+          (merge-sort (before-n array middle))
+          (merge-sort (after-n array middle)))))))
+
+(display (merge-sort '(5 3 4 9 8 1 7 2 0 6)))
+(display "\n")
